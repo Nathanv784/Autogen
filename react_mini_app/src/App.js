6b48be413ce10bcc396ba4441ceaca52e5e38fc7
gitkalaiarasu <kalaiarasus@bitcot.com>
2024-08-07 10:46:17 +0530

    update all code

--- react_mini_app/src/App.js
+++ react_mini_app/src/App.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './App.css'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState("");
   const [messages, setMessages] = useState([]);
-  const [chatStatus, setChatStatus] = useState('ended'); 
+  const [chatStatus, setChatStatus] = useState("ended");
 
   const messagesEndRef = useRef(null);
 
   // // Initial chat request structure
   // const initialChatRequest = {
-  //   "message": "Write a quick manuscript", 
+  //   "message": "Write a quick manuscript",
   //   "agents_info": [
   //       {
   //           "name": "Personal_Assistant",
@@ -35,72 +36,110 @@ function App() {
   const handleSend = async () => {
     let apiEndpoint, requestBody;
 
-    if (chatStatus === 'Chat ongoing' || chatStatus === 'inputting') {
+    if (chatStatus === "Chat ongoing" || chatStatus === "inputting") {
       // Send message request
-      apiEndpoint = 'http://localhost:5008/api/send_message';
+      apiEndpoint = "http://localhost:5008/api/send_message";
       requestBody = { message: userInput };
     } else {
       // Start chat request
-      apiEndpoint = 'http://localhost:5008/api/start_chat';
-      requestBody = {message: userInput }; 
+      apiEndpoint = "http://localhost:5008/api/start_chat";
+      requestBody = { message: userInput };
     }
 
     try {
       const response = await fetch(apiEndpoint, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(requestBody),
       });
-      
+
       if (!response.ok) {
-        throw new Error('Failed to send request');
+        throw new Error("Failed to send request");
       }
 
-      setUserInput(''); // Clear input field
+      setUserInput(""); // Clear input field
     } catch (error) {
-      console.error('Error sending request:', error);
+      console.error("Error sending request:", error);
     }
   };
 
   // Function to fetch messages from the backend
   const fetchMessages = async () => {
     try {
-      const response = await fetch('http://localhost:5008/api/get_message');
+      const response = await fetch("http://localhost:5008/api/get_message");
       if (!response.ok) {
-        throw new Error('Failed to fetch messages');
+        throw new Error("Failed to fetch messages");
       }
 
       const data = await response.json();
       if (data.message) {
         setMessages([...messages, data.message]);
-        
       }
-      setChatStatus(data.chat_status); 
+      setChatStatus(data.chat_status);
     } catch (error) {
-      console.error('Error fetching messages:', error);
+      console.error("Error fetching messages:", error);
     }
   };
 
   // Use useEffect to poll for new messages
   useEffect(() => {
-    const intervalId = setInterval(fetchMessages, 1000); 
+    const intervalId = setInterval(fetchMessages, 1000);
     return () => clearInterval(intervalId);
-  }, [messages]); 
+  }, [messages]);
 
   // Scroll to bottom when new messages are added
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
+  const defalutMessage = [
+    {id:"1",message: "Can you describe the symptoms you've been experiencing?"},
+    {id:"2",message: "How long have you been feeling unwell?"},
+    {id:"3",message: "Can you describe any symptoms you've been experiencing related to your diabetes?"},
+    {id:"4",message: "Have you noticed any chest pain, shortness of breath, or palpitations recently? Collapse"}
+  ]
+
+  const handleHyperLink = (item) => {
+    setUserInput(item);
+  }
+  const handleClear = () => {
+    setUserInput('');
+  }
+
   return (
     <div className="App">
-
       <div className="chat-window">
         <div className="messages">
+          <div className="static-message">
+            <div className="chat-static-contents">
+              <p>
+                Hello! ,Think of me as your patient simulation bot, here to help
+                you practice and refine your medical skills. Please go ahead and
+                ask your questions as if I were your patient. Here are a few
+                questions to get you started:
+              </p>
+            </div>
+            {defalutMessage.map((item,index) => {
+              return(
+                <div className="chat-static-contents-onClick" key={item.id}>
+                  <p onClick={() => {handleHyperLink(item.message)}} style={{ textDecoration: 'underline' }}>{item.message}</p>
+                </div>
+              )
+            })}
+          </div>
           {messages.map((msg, index) => (
-           <div key={index} className={`message ${msg.user === 'Interaction_Agent' ? 'user' : 'agent'}`}>
-           <strong>{msg.user === "Interaction_Agent" ? "" : msg.user} {msg.user === "Interaction_Agent" ? "" : ":"}</strong> {msg.message}
-         </div >
+            <div
+              key={index}
+              className={`message ${
+                msg.user === "Interaction_Agent" ? "user" : "agent"
+              }`}
+            >
+              <strong>
+                {msg.user === "Interaction_Agent" ? "" : msg.user}{" "}
+                {msg.user === "Interaction_Agent" ? "" : ":"}
+              </strong>{" "}
+              {msg.message}
+            </div>
           ))}
           <div ref={messagesEndRef} />
         </div>
@@ -111,7 +150,8 @@ function App() {
             onChange={(e) => setUserInput(e.target.value)}
             placeholder="Type your message..."
           />
-          <button onClick={handleSend}>Send</button> 
+          <button onClick={handleClear} style={{marginRight:'10px'}}>clear</button>
+          <button onClick={handleSend}>Send</button>
         </div>
         <p className="chat-status">Chat Status: {chatStatus}</p>
       </div>
